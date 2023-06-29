c5a5c5d83e695b138637adbceb2d957eaee19435
CandeeLay <candelalay@gmail.com>
2023-06-29 00:18:50 -0300

    Create models in home: user_empleado and user_empresa and empleo

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class user_empleado(models.Model):
+    nombre = models.CharField(max_length = 30)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class user_empresa(models.Model):
+    nombre = models.CharField(max_length = 30)
+    edad = models.IntegerField()
+
+class empleo(models.Model):
+    empleo = models.CharField(max_length = 150)
+    empresa = models.CharField(max_length = 50)
+    sueldo = models.IntegerField()
